7df285f4abb68fc67bf6e5dd8f76cf9b59874898
Seyed Ahmad <seyedahmaddv@gmail.com>
2024-06-02 18:27:59 +0330

    update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,26 +3,26 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts';
-import { 
+import {
   fetchRevenue,
   fetchLatestInvoices,
   fetchCardData,
-  } from '@/app/lib/data';
+} from '@/app/lib/data';
 
 export default async function page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
   } = await fetchCardData();
   return (
     <main>
       <h1 className='{`${lusitana.className} mb-4 text-xl md:text-2xl`}'>
         Dashboard
-        </h1>
+      </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
